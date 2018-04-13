d33cb0503ce6d296594c660657860028f6595b41
Higher Stark <higherwashington@gmail.com>
2018-04-13 14:27:58 +0800

    Add WhatTheNameBean.java

---
+++ src/main/java/com/stark/hello/WhatTheNameBean.java
@@ -0,0 +1,10 @@
+package com.stark.hello;
+
+import org.springframework.beans.factory.BeanNameAware;
+
+public class WhatTheNameBean implements BeanNameAware {
+    private String beanName;
+    public void setBeanName(String beanName) {
+        this.beanName = beanName;
+    }
+}
